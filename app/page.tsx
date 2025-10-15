e2abc1565ace8089cb1df3255202c0d66214515f
TheDigitalMimi <thedigitalmimi@gmail.com>
2025-10-15 03:06:05 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { Package, Pencil, ShoppingCart } from 'lucide-react';
+
+export default function HomePage() {
+  return (
+    <main>
+      <Header />
+      <HeroSection />
+      <FeaturesGrid />
+      <PricingSection />
+      <Footer />
+    </main>
+  );
+}
+
+function Header() { /* ...paste Header code here... */ }
+function HeroSection() { /* ...paste HeroSection code here... */ }
+function FeaturesGrid() { /* ...paste FeaturesGrid code here... */ }
+function PricingSection() { /* ...paste PricingSection code here... */ }
+function Footer() { /* ...paste Footer code here... */ }
